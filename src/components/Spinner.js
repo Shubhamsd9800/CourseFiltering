88c1cb8f5d01d899aff50899a30d28ae82d41e35
Shubham Das <subhamdas980@gmail.com>
2024-03-09 19:25:47 +0530

    Spinner Updated

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { HashLoader } from 'react-spinners';
-import {CSSProperties}  from 'react';
+// import {CSSProperties}  from 'react';
 const Spinner = () => {
   return (
     <div className='flex flex-col items-center space-y-2'>
       <HashLoader
         color={"#FFFFFF"}
         loading={true}
-        cssOverride={CSSProperties}
+        // cssOverride={CSSProperties}
         size={60}
         aria-label="Loading Spinner"
         data-testid="loader"
